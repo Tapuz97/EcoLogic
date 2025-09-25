cf5ded4e24940443207094458dedcaa9ef5f6f9d
Gal mitrani <galmitrani@walla.com>
2025-09-25 13:27:29 +0300

    FUCKING CASE SENSETIVE VITE CONFIG fix

--- vite.config.js
+++ vite.config.js
@@ -13,15 +13,15 @@ export default defineConfig({
         terms: resolve(__dirname, "terms.html"),
         privacy: resolve(__dirname, "privacy.html"),
         notfound: resolve(__dirname, "404.html"),
-        "user-home": resolve(__dirname, "user/home.html"),
-        "user-report": resolve(__dirname, "user/report.html"),
-        "user-shop": resolve(__dirname, "user/shop.html"),
-        "user-profile": resolve(__dirname, "user/profile.html"),
-        "user-camera": resolve(__dirname, "user/camera.html"),
-        "admin-dashboard": resolve(__dirname, "admin/dashboard.html"),
-        "admin-reports": resolve(__dirname, "admin/reports.html"),
-        "admin-analytics": resolve(__dirname, "admin/analytics.html"),
-        "admin-export": resolve(__dirname, "admin/export.html")
+        "user-home": resolve(__dirname, "User/home.html"),
+        "user-report": resolve(__dirname, "User/report.html"),
+        "user-shop": resolve(__dirname, "User/shop.html"),
+        "user-profile": resolve(__dirname, "User/profile.html"),
+        "user-camera": resolve(__dirname, "User/camera.html"),
+        "admin-dashboard": resolve(__dirname, "Admin/dashboard.html"),
+        "admin-reports": resolve(__dirname, "Admin/reports.html"),
+        "admin-analytics": resolve(__dirname, "Admin/analytics.html"),
+        "admin-export": resolve(__dirname, "Admin/export.html")
       }
     }
   }
